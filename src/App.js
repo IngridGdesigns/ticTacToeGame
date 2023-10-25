ad7d27da95e1fe2331edced09fac24b4eaeccb6c
IngridGdesigns <garciai@protonmail.com>
2023-10-25 12:43:06 -0700

    updated type

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function Board() {
     <>
       <div className="board-row">
         <Square value="1"/>
-        <Square vakye="2"/>
+        <Square value="2"/>
         <Square value="3"/>
       </div>
       <div className="board-row">
