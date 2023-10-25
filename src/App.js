9cfa50a036f05d3c47fd30b613e1d2a53851f3f9
IngridGdesigns <garciai@protonmail.com>
2023-10-25 12:49:19 -0700

    added useState, set value to null added onClick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function Square({value}) {
-  return <button className="square">{value}</button>
+import { useState } from 'react';
+
+function Square() {
+  //value storest he value and setValue is a function that can be used to change the value,
+  // null passsed to useState as the initial value for this state variable.
+  // value === null;
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log('clicked!');
+  }
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>);
 }
 
 export default function Board() {
